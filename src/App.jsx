e2b122e2700cb6ab1bbf2f33601c862e2e6aea12
Haridharshini21500176 <vsvel79@gmail.com>
2024-07-04 10:04:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// Import necessary dependencies
+import React from 'react';
+import './App.css';
+import TodoList from './components/Toodo/toodo';
+
+
+function App() {
+  return (
+    <div className="app-container">
+      < TodoList/>
+    </div>
+  );
+}
+
+export default App;
